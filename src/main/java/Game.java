337b195daabc5b06525a9230d33c27e79c59016e
Fumihiko Kinoshita <fkinos@gmail.com>
2017-01-10 23:47:40 +0900

    マジックナンバーを定数に置き換え

--- src/main/java/Game.java
+++ src/main/java/Game.java
@@ -1,18 +1,38 @@
 import java.util.Arrays;
 
 public class Game {
+	static private int GAME_FRAMES = 10;
+	
 	private Frame frame;
 	
 	public Game(int[] fallenPins) {
-		Frame frame = new TenFrame(Arrays.copyOfRange(fallenPins, 18, 22));
+		Frame frame = new TenFrame(Arrays.copyOfRange(fallenPins, GAME_FRAMES * 2 - 2, GAME_FRAMES * 2 + 2));
 		
-		for (int i = 8; i >= 0; i--) {
-			frame = new Frame(Arrays.copyOfRange(fallenPins, i * 2, i * 2 + 2), frame);
+		for (int i = GAME_FRAMES - 1; i > 0; i--) {
+			frame = new Frame(Arrays.copyOfRange(fallenPins, i * 2 - 2, i * 2), frame);
 		}
 		
 		this.frame = frame;
 	}
 	
+	public int[] score() {
+		int[] score = new int[GAME_FRAMES];
+		int prevFrameScore = 0;
+
+		for (int i = 0; i < score.length; i++) {
+			score[i] = frame.score() + prevFrameScore;
+			frame = frame.next();
+			if (frame == null) {
+				// 10フレームまでなかった場合
+				// 例外にしたい
+				break;
+			}
+			prevFrameScore = score[i];
+		}
+		
+		return score;
+	}
+
 	public String toString() {
 		String fallenPins = "";
 		
@@ -26,22 +46,6 @@ public class Game {
 		}
 		
 		return fallenPins;
-	}
-
-	public int[] score() {
-		int[] score = new int[10];
-		int prevFrameScore = 0;
-
-		for (int i = 0; i < score.length; i++) {
-			score[i] = frame.score() + prevFrameScore;
-			frame = frame.next();
-			if (frame == null) {
-				break;
-			}
-			prevFrameScore = score[i];
-		}
-		
-		return score;
 	}
 
 }
