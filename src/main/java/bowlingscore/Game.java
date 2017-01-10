88027260689b0882ab24defcd98163d5e4cfc7e9
Fumihiko Kinoshita <fkinos@gmail.com>
2017-01-11 01:25:56 +0900

    リファクタリング

--- src/main/java/bowlingscore/Game.java
+++ src/main/java/bowlingscore/Game.java
@@ -3,14 +3,19 @@ import java.util.Arrays;
 
 public class Game {
 	static private int GAME_FRAMES = 10;
+	static private int FRAME_THROW_COUNT = 2;
+	static private int TEN_FRAME_THROW_COUNT = 3;
+	static private int TEN_FRAME_THROW_TOTAL_COUNT = (GAME_FRAMES - 1) * FRAME_THROW_COUNT;
 	
 	private Frame frame;
 	
 	public Game(int[] fallenPins) {
-		Frame frame = new TenFrame(Arrays.copyOfRange(fallenPins, GAME_FRAMES * 2 - 2, GAME_FRAMES * 2 + 2));
+		Frame frame = new TenFrame(Arrays.copyOfRange(fallenPins,
+				TEN_FRAME_THROW_TOTAL_COUNT, TEN_FRAME_THROW_TOTAL_COUNT + TEN_FRAME_THROW_COUNT + 1));
 		
 		for (int i = GAME_FRAMES - 1; i > 0; i--) {
-			frame = new Frame(Arrays.copyOfRange(fallenPins, i * 2 - 2, i * 2), frame);
+			frame = new Frame(Arrays.copyOfRange(fallenPins,
+					(i - 1) * FRAME_THROW_COUNT, i * FRAME_THROW_COUNT), frame);
 		}
 		
 		this.frame = frame;
@@ -18,17 +23,17 @@ public class Game {
 	
 	public int[] score() {
 		int[] score = new int[GAME_FRAMES];
-		int prevFrameScore = 0;
+		int totalScore = 0;
 
 		for (int i = 0; i < score.length; i++) {
-			score[i] = frame.score() + prevFrameScore;
+			score[i] = frame.score() + totalScore;
 			frame = frame.next();
 			if (frame == null) {
 				// 10フレームまでなかった場合
 				// 例外にしたい
 				break;
 			}
-			prevFrameScore = score[i];
+			totalScore = score[i];
 		}
 		
 		return score;
